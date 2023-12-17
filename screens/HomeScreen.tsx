d504f9aae2bf55ce6ca397ae7ca8dc984598940d
Oikake <nguyenlyphat301202@gmail.com>
2023-12-18 05:05:57 +0700

    Add home screen to app

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
