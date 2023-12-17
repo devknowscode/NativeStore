909cf52e06ef370f49fb857305169f6625b3ee63
Oikake <nguyenlyphat301202@gmail.com>
2023-12-18 05:10:31 +0700

    Add user account screen to app

---
+++ screens/AccountScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const AccountScreen = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Text>AccountScreen</Text>
+    </View>
+  );
+};
+
+export default AccountScreen;
