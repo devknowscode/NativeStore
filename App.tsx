27bc53a3a31cf4e1c666d628dd886902a6aedb5a
NguyenMinhTrung20522072 <20522072@gm.uit.edu.vn>
2023-12-17 19:19:15 +0700

    change something

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to React Native</Text>
       </Box>
     </NativeBaseProvider>
   );
