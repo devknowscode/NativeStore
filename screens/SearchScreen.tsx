d518ac1c1303e5d32866a4e1dd5d93315893e4d3
Oikake <nguyenlyphat301202@gmail.com>
2023-12-18 05:06:39 +0700

    Add searching screen to app

---
+++ screens/SearchScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const SearchScreen = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
