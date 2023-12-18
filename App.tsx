60b39785b06ea62f6bf001b2629e683f27971ba6
NguyenMinhTrung20522072 <20522072@gm.uit.edu.vn>
2023-12-18 22:00:54 +0700

    Add theme and use native base to app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-// import {NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider, extendTheme} from 'native-base';
 import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
+  const theme = extendTheme({
+    components: {
+      Button: {
+        variants: {
+          default: ({colorScheme}) => {
+            return {
+              bg: `${colorScheme}.500`,
+              rounded: 20,
+            };
+          },
+
+          outline: ({colorScheme}) => {
+            return {
+              borderColor: `${colorScheme}.500`,
+              rounded: 20,
+            };
+          },
+        },
+      },
+    },
+  });
+
   return (
-    // <NativeBaseProvider>
-      <LoginScreen/>
-    // </NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
+      <LoginScreen />
+    </NativeBaseProvider>
   );
 }
